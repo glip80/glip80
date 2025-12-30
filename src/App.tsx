0190a0e84f193dae611db30190e9e46d49f0695c
Alex Polyakov <alex.polyakov@cellebrite.com>
2025-12-30 15:45:05 +0200

    feat: initial commit - profile page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ProfileCard } from './components/ProfileCard'
+
+function App() {
+    return (
+        <div className="min-h-screen w-full flex items-center justify-center p-4">
+            <ProfileCard />
+        </div>
+    )
+}
+
+export default App
