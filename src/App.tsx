e53688b78d0094508b2d45ba314bae28727d61af
Alex Polyakov <alex.polyakov@cellebrite.com>
2025-12-30 16:04:05 +0200

    feat: add initial application structure with global styles and ProfileCard component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ProfileCard } from './components/ProfileCard'
 
 function App() {
     return (
-        <div className="min-h-screen w-full flex items-center justify-center p-4">
+        <div className="app-container">
             <ProfileCard />
         </div>
     )
